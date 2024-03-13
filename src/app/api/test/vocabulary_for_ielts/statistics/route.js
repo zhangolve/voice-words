63aafbdf80fa9545dd581948ef252b45c7ec82c2
Olive Zhang <xingwang.zhang@everbridge.com>
2024-03-13 16:24:54 +0800

    enhance statistics

---
+++ src/app/api/test/vocabulary_for_ielts/statistics/route.js
@@ -0,0 +1,17 @@
+// 先不做自定义word list 
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+
+export async function GET() {
+    try {
+        const result = await sql`SELECT
+        COUNT(CASE WHEN mastered IS TRUE THEN 1 END) AS mastered,
+        COUNT(CASE WHEN mastered IS FALSE THEN 1 END) AS learning,
+        COUNT(CASE WHEN mastered IS NULL THEN 1 END) AS ready
+    FROM vocabulary_for_ielts;`;
+        return NextResponse.json({ result: result.rows[0] }, { status: 200 });
+    } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+    }
+}
--- src/app/test/page.js
+++ src/app/test/page.js
@@ -11,6 +11,7 @@ const Review = () => {
     const [words, setWords] = useState([])
     const [currentWord, setCurrentWord] = useState()
     const {data, error, isLoading} = useMySWR({url:'/api/test/vocabulary_for_ielts'})
+    const {data:statistics, error:statisticsError, mutate} = useMySWR({url:'/api/test/vocabulary_for_ielts/statistics'})
 
     useEffect(()=>{
         if(!data) return;
@@ -24,17 +25,19 @@ const Review = () => {
     const onRetry = async () => {
         await retry(currentWord);
         setWords(words.slice(1))
+        mutate({...statistics, result: {...statistics.result, learning: statistics.result.ready+1, ready: statistics.result.ready-1}});
     }
 
     const onMaster = async () => {
         await master(currentWord);
         setWords(words.slice(1))
+        mutate({...statistics, result: {...statistics.result, mastered: statistics.result.ready+1, ready: statistics.result.ready-1}});
     }
 
     if(isLoading) return <Loading />
-
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {statistics && <div>mastered: {statistics.result.mastered} learning: {statistics.result.learning} ready: {statistics.result.ready}</div>}
             {currentWord &&
             <div className='review'>
                 <ReviewCard word={currentWord} key={currentWord.word} />
