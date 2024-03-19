66bdd7422b4562db3fcde305b2614d214d526de5
Olive Zhang <xingwang.zhang@everbridge.com>
2024-03-19 14:25:07 +0800

    better tts

--- src/app/api/tts/route.js
+++ src/app/api/tts/route.js
@@ -32,58 +32,32 @@ const textToSpeech = async (key, region, text, filename)=> {
 
         
         let audioConfig = null;
+      
         
-        if (filename) {
-            audioConfig = sdk.AudioConfig.fromAudioFileOutput(filename);
-        }
-        
-        const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-        synthesizer.speakTextAsync(
-            text,
-            result => {
-                if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-                    console.log("synthesis finished");
-                    resolve();
-                } else if (result.reason === SpeechSDK.ResultReason.Canceled) {
-                    console.log("synthesis failed. Error detail: " + result.errorDetails);
-                  }
-                synthesizer.close();
-                
-                
-                if (filename) {
-                    // console.log('finish')
-                    // return stream from file
-                    // const audioFile = fs.createReadStream(filename);
-                    // resolve(audioFile);
-                    
-                } else {
-                    const bufferStream = new PassThrough();
-                    bufferStream.end(Buffer.from(audioData));
-                    resolve(bufferStream);
-                }
-               
-                if (filename) {
-                    // console.log('finish')
-                    // return stream from file
-                    // const audioFile = fs.createReadStream(filename);
-                    // resolve(audioFile);
-                    
-                } else {
-                    const bufferStream = new PassThrough();
-                    bufferStream.end(Buffer.from(audioData));
-                    resolve(bufferStream);
-                }
-            },
-            error => {
-                console.log('error', error);
-                synthesizer.close();
-                reject(error);
-            }); 
+        // const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+        const synthesizer = new sdk.SpeechSynthesizer(speechConfig);
 
-            // synthesizer.synthesisCompleted = function (s, e) {
-              
-            // };
+        // 监听合成事件
+        synthesizer.synthesisCompleted = (s, e) => {
+            if (e.result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+                console.log('语音合成完成');
+        
+                // 将合成的音频保存到文件
+                console.log(`音频要保存到 ${filename}`);
+                const buffer = Buffer.from(e.result.audioData);
+                fs.writeFileSync(filename, buffer);
+                console.log(`音频已保存到 ${filename}`);
+                resolve();
+            } else {
+                console.error('合成失败:', e.result.reason);
+            }
+        };
+        
+        // 开始合成文本为语音
+        console.log('开始合成...');
+        synthesizer.speakTextAsync(text);
     });
+        
 };
 
 
@@ -96,6 +70,7 @@ export async function POST(req) {
     const fileName = `${word.replace(' ','_')}-${now}.mp3`;
     const filePath = process.env.VERCEL_ENV !== 'development'? `/tmp/${fileName}`:`temp/${fileName}` ;
     await textToSpeech(key, region, text, filePath);
+    console.log('filePath',filePath)
     const blob = await bucket.uploadFile(filePath, fileName, undefined ,"audio/mpeg");
     return NextResponse.json(blob);
 }
